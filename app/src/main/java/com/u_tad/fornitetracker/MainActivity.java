0eb63af7cfbe2eb8a82cc1b04fa4154de71c5512
Primc <pablocruzmendez@gmail.com>
2018-11-27 10:47:29 +0100

    checkepoint

--- app/src/main/java/com/u_tad/fornitetracker/MainActivity.java
+++ app/src/main/java/com/u_tad/fornitetracker/MainActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
@@ -13,19 +16,47 @@ public class MainActivity extends AppCompatActivity {
 
     private AdapterFornitePlayers adapter;
     private RecyclerView recyclerView;
+    private Button search;
+    private EditText userName;
+    private ArrayList<ForniteUser> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setViewStuffs();
+
+        loadView(arrayList);
+
+        search();
+
+        getData("xPrimcx");
+    }
+
+    public void setViewStuffs(){
+        recyclerView = findViewById(R.id.recycler_view_notice_list);
+        search = findViewById(R.id.button_search);
+        userName = findViewById(R.id.textView_user);
     }
 
     public void loadView(ArrayList<ForniteUser> arrayList){
-        recyclerView = findViewById(R.id.recycler_view_notice_list);
+
         adapter = new AdapterFornitePlayers(arrayList);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
+    }
+
+    private void search() {
+        search.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                getData(userName.getText().toString());
+            }
+        });
+    }
+
+    public void getData(String userNickName){
+
     }
 }
